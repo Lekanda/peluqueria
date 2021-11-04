70f9392f9279aba27f38742250a231ee12cebc5e
lekanda <abernaolao@gmail.com>
2021-11-04 18:04:27 +0100

    Mostrar nombre de usuario autenticado en formulario para nueva cita .

--- controllers/CitaController.php
+++ controllers/CitaController.php
@@ -7,9 +7,14 @@ use MVC\Router;
 class CitaController{
     public static function index(Router $router){
 
-        
+        if(!isset($_SESSION)){ 
+            session_start(); 
+        } 
+        $nombre = ($_SESSION['nombre']);
+
+
         $router->render('cita/index',[
-            
+            'nombre' => $nombre
         ]);
     }
 
--- views/cita/index.php
+++ views/cita/index.php
@@ -4,15 +4,42 @@
 <div id="app">
     <div id="paso-1" class="seccion">
         <h2>Servicios</h2>
-        <p>Elige tus servicios a continuación</p>
+        <p class="text-center">Elige tus servicios a continuación</p>
         <div id="servicios" class="listado-servicios"></div>
     </div>
     <div id="paso-2" class="seccion">
         <h2>Tus datos y Cita</h2>
-        <p>Coloca tus datos y fecha de tu cita</p>
+        <p class="text-center">Coloca tus datos y fecha de tu cita</p>
+
+        <form class="formulario" action="">
+            <div class="campo">
+                <label for="nombre">Nombre</label>
+                <input 
+                    id="nombre"
+                    type="text"
+                    placeholder="Tu nombre"
+                    value="<?php echo $nombre; ?>"
+                    disabled
+                />
+            </div>
+            <div class="campo">
+                <label for="fecha">Fecha</label>
+                <input 
+                    id="fecha"
+                    type="date"
+                />
+            </div>
+            <div class="campo">
+                <label for="hora">Hora</label>
+                <input 
+                    id="hora"
+                    type="time"
+                />
+            </div>
+        </form>
     </div>
     <div id="paso-3" class="seccion">
         <h2>Resumen</h2>
-        <p>Verfica que la informacion sea correcta</p>
+        <p class="text-center">Verfica que la informacion sea correcta</p>
     </div>
 </div>
