a4702180c1979c0e2f04b77b33ff6e05f2bdad34
lekanda <abernaolao@gmail.com>
2021-11-04 17:42:31 +0100

    Añadiendo contenedores principales. HTML de index de citas (/cita).

--- views/cita/index.php
+++ views/cita/index.php
@@ -1,3 +1,18 @@
 <h1 class="nombre-pagina">Crear Cita</h1>
-<p class="descripcion-pagina">Elije tus servicios a continuacion</p>
+<p class="descripcion-pagina">Elije tus servicios a continuacion y pon tus datos</p>
 
+<div id="app">
+    <div id="paso-1" class="seccion">
+        <h2>Servicios</h2>
+        <p>Elige tus servicios a continuación</p>
+        <div id="servicios" class="listado-servicios"></div>
+    </div>
+    <div id="paso-2" class="seccion">
+        <h2>Tus datos y Cita</h2>
+        <p>Coloca tus datos y fecha de tu cita</p>
+    </div>
+    <div id="paso-3" class="seccion">
+        <h2>Resumen</h2>
+        <p>Verfica que la informacion sea correcta</p>
+    </div>
+</div>
