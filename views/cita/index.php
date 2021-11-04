dfc942e2e540e4805f82e6294e2daedcdd77e825
lekanda <abernaolao@gmail.com>
2021-11-04 18:41:09 +0100

    Botones para paginador en Crear cita. /cita.

--- views/cita/index.php
+++ views/cita/index.php
@@ -16,6 +16,7 @@
         <p class="text-center">Elige tus servicios a continuación</p>
         <div id="servicios" class="listado-servicios"></div>
     </div>
+
     <div id="paso-2" class="seccion">
         <h2>Tus datos y Cita</h2>
         <p class="text-center">Coloca tus datos y fecha de tu cita</p>
@@ -47,8 +48,20 @@
             </div>
         </form>
     </div>
+
     <div id="paso-3" class="seccion">
         <h2>Resumen</h2>
         <p class="text-center">Verfica que la informacion sea correcta</p>
+    </div>
+
+    <div class="paginacion">
+        <button
+            id="anterior"
+            class="boton"
+        >&laquo; Anterior</button>
+        <button
+            id="siguiente"
+            class="boton"
+        >Siguiente &raquo;</button>
     </div>
 </div>
