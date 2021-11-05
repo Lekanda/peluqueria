0215ba39d2181bb014ff657872c6742b724d5a79
lekanda <abernaolao@gmail.com>
2021-11-05 11:30:40 +0100

    Crear modelo de Servicios y exportar datos a JSON.

---
+++ controllers/APIController.php
@@ -0,0 +1,17 @@
+<?php 
+
+namespace Controllers;
+
+use Model\Servicio;
+
+class APIController{
+    public static function index(){
+        $servicios = Servicio::all();
+        echo json_encode($servicios);
+    }
+}
+
+
+
+
+
--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
+use Controllers\APIController;
 use Controllers\CitaController;
 use Controllers\LoginController;
 
@@ -34,10 +35,12 @@ $router->get('/confirmar-cuenta',[LoginController::class, 'confirmar']);
 
 /********************* AREA PRIVADA ************************/
 $router->get('/cita',[CitaController::class, 'index']);
+/***********************************************************/
 
 
 
-
+/********************* API CITAS ************************/
+$router->get('/api/servicios',[APIController::class, 'index']);
 
 
 
