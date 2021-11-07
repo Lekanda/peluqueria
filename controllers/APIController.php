f92f87ec5c1f61eb58c54cddeb7fe5a01b6ea19f
lekanda <abernaolao@gmail.com>
2021-11-07 17:43:31 +0100

    Guardando los servicios  y la cita en tabla citasservicios.

--- controllers/APIController.php
+++ controllers/APIController.php
@@ -4,6 +4,7 @@ namespace Controllers;
 
 use Model\Cita;
 use Model\Servicio;
+use Model\CitaServicio;
 
 class APIController{
     public static function index(){
@@ -17,10 +18,24 @@ class APIController{
 
     // Metodo para guardar citas desde app.js
     public static function guardar(){
+        // Almacena la cita y devuelve el id
         $cita = new Cita($_POST);
         $resultado = $cita->guardar();
 
-        echo json_encode($resultado);
+        $id = $resultado['id'];// Id de la cita creada
+
+        // Almacena la cita y el servicio
+        $idServicios = explode(",", $_POST['servicios']);
+        // Iteramos egun el numero  de servicios elegidos y le ponemos la misma citaId
+        foreach($idServicios as $idServicio){
+            $args = [
+                'citaId' => $id,
+                'servicioId' => $idServicio
+            ];
+            $citaServicio = new CitaServicio($args); // Creamos el objeto para tabla 'citasservicios'(Pivote)
+            $citaServicio->guardar(); // Guardamos en tabla 'citasservicios'
+        }
+        echo json_encode(['resultado' => $resultado]);
     }
 }
 
--- models/ActiveRecord.php
+++ models/ActiveRecord.php
@@ -143,7 +143,7 @@ class ActiveRecord {
         $query .= join("', '", array_values($atributos));
         $query .= " ') ";
 
-        return json_encode(['query' => $query]);
+        // return json_encode(['query' => $query]);
 
         // Resultado de la consulta
         $resultado = self::$db->query($query);
