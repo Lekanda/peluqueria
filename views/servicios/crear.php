39354e49cdcc6497586153c2ef6bac8fd25f2e99
lekanda <abernaolao@gmail.com>
2021-11-09 15:58:56 +0100

    Formulario para servicios.

--- views/servicios/crear.php
+++ views/servicios/crear.php
@@ -4,3 +4,9 @@
 <?php 
     include_once __DIR__ . '/../templates/barra.php';
 ?>
+
+<form class="formulario" action="/servicios/crear" method="POST">
+    <?php include_once __DIR__ . '/formulario.php'; ?>
+
+    <input type="submit" class="boton" value="Crear Servicio">
+</form>
