0465868c88d3e85ef34a25ed54fe7cf619829202
lekanda <abernaolao@gmail.com>
2021-11-09 15:27:03 +0100

    Vistas para los servicios.

--- controllers/ServicioController.php
+++ controllers/ServicioController.php
@@ -6,32 +6,54 @@ use MVC\Router;
 
 class ServicioController{
 
+
     public static function index(Router $router){
-        
+        if(!isset($_SESSION)){ 
+            session_start(); 
+        } 
         $router->render('servicios/index',[
+            'nombre' => $_SESSION['nombre'],
 
         ]);
     }
 
 
+
+
     public static function crear(Router $router){
+        if(!isset($_SESSION)){ 
+            session_start(); 
+        } 
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             
         }
 
-        $router->render('servicios/crear',[]);
+        $router->render('servicios/crear',[
+            'nombre' => $_SESSION['nombre']
+        ]);
     }
+
+
 
 
     public static function actualizar(Router $router){
+        if(!isset($_SESSION)){ 
+            session_start(); 
+        } 
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             
         }
 
-        $router->render('servicios/actualizar',[]);
+        $router->render('servicios/actualizar',[
+            'nombre' => $_SESSION['nombre']
+        ]);
     }
 
 
+
+
+
+    
     public static function eliminar(Router $router){
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             
---
+++ views/servicios/crear.php
@@ -0,0 +1,6 @@
+<h1 class="nombre-pagina">Crear Servicios</h1>
+<p class="descripcion-pagina">Creacion de Servicios</p>
+
+<?php 
+    include_once __DIR__ . '/../templates/barra.php';
+?>
